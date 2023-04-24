5cdbfd6e7ebfc2750866b3d5ac58a9f4f19754e4
zhuyaotong <zhuyaotong@foxmail.com>
2023-04-24 23:32:31 +0800

    Java并发编程实战

--- concurrency/src/main/java/com/github/zhuyaotong/concurrency/Singleton.java
+++ concurrency/src/main/java/com/github/zhuyaotong/concurrency/Singleton.java
@@ -124,3 +124,100 @@ class SafeCalc3 {
     }
   }
 }
+
+class Account {
+
+  // 锁：保护账户余额（细粒度锁）
+  private final Object balLock = new Object();
+
+  // 账户余额
+  private Integer balance;
+
+  // 锁：保护账户密码（细粒度锁）
+  private final Object pwLock = new Object();
+
+  // 账户密码
+  private String password;
+
+  // 取款
+  void withdraw(Integer amt) {
+
+    synchronized (balLock) {
+      if (this.balance > amt) {
+        this.balance -= amt;
+      }
+    }
+  }
+  // 查看余额
+  Integer getBalance() {
+    synchronized (balLock) {
+      return balance;
+    }
+  }
+
+  // 更改密码
+  void updatePassword(String pw) {
+    synchronized (pwLock) {
+      this.password = pw;
+    }
+  }
+
+  // 查看密码
+  String getPassword() {
+    synchronized (pwLock) {
+      return password;
+    }
+  }
+}
+
+class Account2 {
+
+  private int balance;
+
+  // 转账
+  synchronized void transfer(Account2 target, int amt) {
+
+    if (this.balance > amt) {
+      this.balance -= amt;
+      target.balance += amt;
+    }
+  }
+}
+
+class Account3 {
+  private Object lock;
+  private int balance;
+
+  private Account3() {}
+
+  // 创建Account时传入同一个lock对象
+  public Account3(Object lock) {
+    this.lock = lock;
+  }
+
+  // 转账
+  void transfer(Account3 target, int amt) {
+
+    // 此处检查所有对象共享的锁
+    synchronized (lock) {
+      if (this.balance > amt) {
+        this.balance -= amt;
+        target.balance += amt;
+      }
+    }
+  }
+}
+
+class Account4 {
+  private int balance;
+  // 转账
+  void transfer(Account4 target, int amt) {
+
+    synchronized (Account4.class) {
+      if (this.balance > amt) {
+        this.balance -= amt;
+        target.balance += amt;
+      }
+    }
+  }
+}
