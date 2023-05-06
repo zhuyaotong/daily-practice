82bfbcc5402f214a1ee660e6122dd9747f0665c3
zhuyaotong <zhuyaotong@foxmail.com>
2023-05-06 22:39:22 +0800

    Java并发编程实战

--- concurrency/src/main/java/com/github/zhuyaotong/concurrency/ObjPool.java
+++ concurrency/src/main/java/com/github/zhuyaotong/concurrency/ObjPool.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -130,5 +130,74 @@ class SL {
     } finally {
       sl.unlock(stamp);
     }
+  }
+}
+
+// T1Task需要执行的任务：
+// 洗水壶、烧开水、泡茶
+class T1Task implements Callable<String> {
+
+  FutureTask<String> ft2;
+
+  // T1任务需要T2任务的FutureTask
+  T1Task(FutureTask<String> ft2) {
+    this.ft2 = ft2;
+  }
+
+  @Override
+  public String call() throws Exception {
+
+    System.out.println("T1:洗水壶...");
+    TimeUnit.SECONDS.sleep(1);
+
+    System.out.println("T1:烧开水...");
+    TimeUnit.SECONDS.sleep(15);
+
+    // 获取T2线程的茶叶
+    String tf = ft2.get();
+    System.out.println("T1:拿到茶叶:" + tf);
+
+    System.out.println("T1:泡茶...");
+
+    return "上茶:" + tf;
+  }
+
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    // 创建任务T2的FutureTask
+    FutureTask<String> ft2 = new FutureTask<>(new T2Task());
+
+    // 创建任务T1的FutureTask
+    FutureTask<String> ft1 = new FutureTask<>(new T1Task(ft2));
+
+    // 线程T1执行任务ft1
+    Thread T1 = new Thread(ft1);
+    T1.start();
+
+    // 线程T2执行任务ft2
+    Thread T2 = new Thread(ft2);
+    T2.start();
+
+    // 等待线程T1执行结果
+    System.out.println(ft1.get());
+  }
+}
+
+// T2Task需要执行的任务:
+// 洗茶壶、洗茶杯、拿茶叶
+class T2Task implements Callable<String> {
+
+  @Override
+  public String call() throws Exception {
+
+    System.out.println("T2:洗茶壶...");
+    TimeUnit.SECONDS.sleep(1);
+
+    System.out.println("T2:洗茶杯...");
+    TimeUnit.SECONDS.sleep(2);
+
+    System.out.println("T2:拿茶叶...");
+    TimeUnit.SECONDS.sleep(1);
+
+    return "龙井";
   }
 }
