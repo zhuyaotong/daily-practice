bbd2072db9b9b23594afaa47148715bf04595541
zhuyaotong <zhuyaotong@foxmail.com>
2023-05-10 10:13:15 +0800

    Java并发编程实战

--- concurrency/src/main/java/com/github/zhuyaotong/concurrency/A.java
+++ concurrency/src/main/java/com/github/zhuyaotong/concurrency/A.java
@@ -5,15 +5,19 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class A {
 
   public static void main(String[] args) throws IOException {
 
-    final ServerSocketChannel ssc = ServerSocketChannel.open().bind(new InetSocketAddress(8080));
-
     // 处理请求
-    try {
+    try (ServerSocketChannel ssc = ServerSocketChannel.open().bind(new InetSocketAddress(8080))) {
       while (true) {
         // 接收请求
         SocketChannel sc = ssc.accept();
@@ -42,9 +46,58 @@ public class A {
                 })
             .start();
       }
-
-    } finally {
-      ssc.close();
     }
+  }
+}
+
+class ExecTasks {
+  private static class Task {}
+
+  // 任务队列
+  BlockingQueue<Task> bq = new LinkedBlockingQueue<>(2000);
+
+  // 启动5个消费者线程
+  // 执行批量任务
+  void start() {
+    ExecutorService es = Executors.newFixedThreadPool(5);
+
+    for (int i = 0; i < 5; i++) {
+      es.execute(
+          () -> {
+            try {
+              while (true) {
+                // 获取批量任务
+                List<Task> ts = pollTasks();
+
+                // 执行批量任务
+                execTasks(ts);
+              }
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+          });
+    }
+  }
+
+  // 从任务队列中获取批量任务
+  List<Task> pollTasks() throws InterruptedException {
+    List<Task> ts = new LinkedList<>();
+
+    // 阻塞式获取一条任务
+    Task t = bq.take();
+
+    while (t != null) {
+      ts.add(t);
+
+      // 非阻塞式获取一条任务
+      t = bq.poll();
+    }
+
+    return ts;
+  }
+
+  // 批量执行任务
+  void execTasks(List<Task> ts) {
+    // 省略具体代码无数
   }
 }
